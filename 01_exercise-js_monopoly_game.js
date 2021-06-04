97857de01d8e26ffd3ae56312740f77785a78c5c
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-04 11:58:05 +0200

    Fnished OOP exercise

--- 01_exercise-js_monopoly_game.js
+++ 01_exercise-js_monopoly_game.js
@@ -22,8 +22,68 @@ const squares = [
   100, -10, 0, 0, -40, -10, -10, 5, 0, -10, -50, -10, 0, 0, -50, -10,
 ];
 
-// --- Initialization ---
+// Creation of the class
+class Player {
+  // The constructor is the method triggered with the `new` keyword
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+    this.position = 0;
+    this.cash = 1000;
+  }
 
+  // Method move
+  move() {
+    let dice = 1 + Math.floor(6 * Math.random());
+    console.log(`${this.name} rolled ${dice}`);
+    this.position = (this.position + dice) % squares.length;
+    this.cash += squares[this.position];
+    if (this.cash < 0) {
+      console.log(`Game over for ${this.name}.`);
+    }
+    console.log(`${this.name} is at ${this.position}`);
+  }
+
+  // Method displayInfo
+  displayInfo() {
+    console.log(
+      `${this.name} is at position ${this.position} and has ${this.cash}€`
+    );
+  }
+}
+// --- Initialization of player ---
+
+const player1 = new Player("Joaquim", "red");
+const player2 = new Player("Maxence", "blue");
+const player3 = new Player("Mostafa", "black");
+
+// --- Turn 1  ---
+player1.move();
+player2.move();
+player3.move();
+
+// --- Turn 2  ---
+player1.move();
+player2.move();
+player3.move();
+
+// --- Turn 3  ---
+player1.move();
+player2.move();
+player3.move();
+
+// --- Turn 4  ---
+player1.move();
+player2.move();
+player3.move();
+
+// --- Display info  ---
+player1.displayInfo();
+player2.displayInfo();
+player3.displayInfo();
+
+/*
+// --- Initialization ---
 let player1 = {
   name: "Joaquim",
   color: "red",
@@ -86,6 +146,56 @@ let player3 = {
     );
   },
 };
+
+// --- Turn 1  ---
+player1.move();
+player2.move();
+player3.move();
+
+// --- Turn 2  ---
+player1.move();
+player2.move();
+player3.move();
+
+// --- Turn 3  ---
+player1.move();
+player2.move();
+player3.move();
+
+// --- Turn 4  ---
+player1.move();
+player2.move();
+player3.move();
+
+// --- Display info  ---
+player1.displayInfo();
+player2.displayInfo();
+player3.displayInfo();
+*/
+
+/*
+// --- Initialization ---
+
+let player1 = {
+  name: "Joaquim",
+  color: "red",
+  position: 0,
+  cash: 1000
+};
+
+let player2 = {
+  name: "Maxence",
+  color: "blue",
+  position: 0,
+  cash: 1000,
+};
+
+let player3 = {
+  name: "Mostafa",
+  color: "black",
+  position: 0,
+  cash: 1000,
+};
 let dice;
 // --- Turn of Player 1 ---
 // The dice is a random integer between 1 and 6
@@ -122,3 +232,4 @@ if (player3.cash < 0) {
 console.log(player1);
 console.log(player2);
 console.log(player3);
+ */
